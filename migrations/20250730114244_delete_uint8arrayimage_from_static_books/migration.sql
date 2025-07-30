424fcb016132e222a3a15d8b59fe1bc6b4dec39d
s-hirano-ist <s-hirano-ist@outlook.com>
2025-07-30 20:42:58 +0900

    feat: remove uint8ArrayImage column from StaticBooks model

---
+++ migrations/20250730114244_delete_uint8arrayimage_from_static_books/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `uint8ArrayImage` on the `st_books` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."st_books" DROP COLUMN "uint8ArrayImage";
